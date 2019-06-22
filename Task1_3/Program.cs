aea3f408eaa3f7ea160bc260963dc246abe7544c
AndreiKotM <andrew.m.kot@gmail.com>
2019-06-22 21:03:34 +0300

    Separate a project on UI and Logic projects

--- Task1_3/Program.cs
+++ Task1_3/Program.cs
@@ -336,68 +336,6 @@ namespace Task1_3
                 Console.WriteLine(menuOption[i]);
             }
             
-        }
-
-        //Merge Sort Interface
-        public static List<int> MergeSort(List<int> unsorted)
-        {
-            List<int> sorted = new List<int> {};
-
-            if (unsorted.Count <= 1)
-            {
-                sorted = unsorted;
-                return sorted;
-            }
-
-            int midle = unsorted.Count / 2;
-
-            List<int> Left = new List<int> { };
-            List<int> Right = new List<int> { };
-
-            for (int i = 0; i < midle; i++)
-            {
-                Left.Add(unsorted[i]);
-            }
-
-            for (int i = midle; i < unsorted.Count; i++)
-            {
-                Right.Add(unsorted[i]);
-            }
-
-            Left = MergeSort(Left);
-            Right = MergeSort(Right);
-
-            return Merge(Left, Right);
-        }
-
-        public static List<int> Merge(List<int> Left, List<int> Right)
-        {
-            List<int> result = new List<int> { };
-
-            while (Left.Count != 0 && Right.Count != 0)
-            {
-                if (Left[0] >= Right[0])
-                {
-                    result.Add(Right[0]);
-                    Right.Remove(Right[0]);
-                }
-                else
-                {
-                    result.Add(Left[0]);
-                    Left.Remove(Left[0]);
-                }
-            }
-
-            if (Left.Count == 0)
-            {
-                result.AddRange(Right);
-            }
-            else
-            {
-                result.AddRange(Left);
-            }
-
-            return result;
-        }
+        } 
     }
 }
